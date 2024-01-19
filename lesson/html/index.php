062cdd3a5a265edb55ea11c2ea5c3df38dd5f53d
keikatakagi <keika.takagi@gmail.com>
2024-01-19 18:20:27 +0900

    PHP環境設定0119

---
+++ lesson/html/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+  <?php
+  $message = "今日の日付：" . date('Y/m/d');
+  ?>
+
+  <h1>Hello PHP World!</h1>
+
+  <?= $message ?>
+
+</body>
+</html>
