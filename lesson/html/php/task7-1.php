a613806b0884c6ab950bcb05461c292383b5ba94
keikatakagi <keika.takagi@gmail.com>
2024-01-31 22:07:31 +0900

    PHP基礎知識⑦課題

---
+++ lesson/html/php/task7-1.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<?php
+  ini_set('display_errors', 1);
+  
+  class Staff{
+    private $name;
+    private $age;
+    private $sex;
+    private static $id = 0;
+
+    public function __construct($name, $age, $sex){
+      $this -> name = $name;
+      $this -> age = $age;
+      $this -> sex = $sex;
+      $this -> number();
+    }
+    public function number(){
+      self::$id++;
+    }
+    public function show(){
+      printf('(S%04d) %s %s歳 %s性<br>', self::$id, $this -> name, $this -> age, $this -> sex);
+    }
+  }
+
+  $staff =[];
+  $staff[0] = new Staff('佐藤　一郎', '31', '男');
+  $staff[0]->show();
+
+  $staff[1] = new Staff('山田　花子', '25', '女');
+  $staff[1]->show();
+
+  $staff[2] = new Staff('鈴木　次郎', '27', '男');
+  $staff[2]->show();
+  
+  ?>
+</body>
+</html>
---
+++ lesson/html/php/task7-2.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<?php
+  ini_set('display_errors', 1);
+  
+  class Staff{
+    private $name;
+    private $age;
+    private $sex;
+    protected static $id = 0;
+
+    public function __construct($name, $age, $sex){
+      $this -> name = $name;
+      $this -> age = $age;
+      $this -> sex = $sex;
+      $this -> number();
+    }
+    protected function number(){
+      self::$id++;
+    }
+    public function show(){
+      printf('(S%04d) %s %s歳 %s性<br>', self::$id, $this -> name, $this -> age, $this -> sex);
+    }
+  }
+
+  class PartStaff extends Staff{
+    private $jikyu;
+
+    public function __construct($name, $age, $sex,$jikyu){
+      $this -> name = $name;
+      $this -> age = $age;
+      $this -> sex = $sex;
+      $this -> jikyu = $jikyu;
+      $this -> number();
+    }
+    public function number(){
+      self::$id++;
+    }
+    public function show(){
+      printf('(P%04d) %s %s歳 %s性 時給：%d円<br>', self::$id, $this -> name, $this -> age, $this -> sex, $this-> jikyu);
+    }
+  }
+
+  $staff =[];
+  $staff[0] = new Staff('佐藤　一郎', '31', '男');
+  $staff[0]->show();
+
+  $staff[1] = new Staff('山田　花子', '25', '女');
+  $staff[1]->show();
+
+  $staff[2] = new Staff('鈴木　次郎', '27', '男');
+  $staff[2]->show();
+
+  $staff[3] = new PartStaff('田中　友子', '24', '女','900');
+  $staff[3]->show();
+
+  $staff[4] = new Staff('中村　三郎', '27', '男');
+  $staff[4]->show();
+  
+
+  ?>
+</body>
+</html>
