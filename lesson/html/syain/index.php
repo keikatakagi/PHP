401bc2b358e113cc09199feef0381cc180a20090
keikatakagi <keika.takagi@gmail.com>
2024-02-23 22:34:46 +0900

    PHP基礎知識⑩
    post_data.php 23行目 error 未定義
    Database.php 12行目・42行目 pdo 未定義、関数prepare()呼び出しエラー

--- lesson/html/syain/index.php
+++ lesson/html/syain/index.php
@@ -1,9 +1,10 @@
 <?php
-require_once('app/Database.php');
-require_once('app/html_func.php');
-$db = new Database();
+
+ini_set('display_errors', "On");
+
+require_once('common.php');
 // $members = $db->getallsyain();
 show_top();
-show_syainlist($members);
+// show_syainlist($members);
 show_down();
 ?>
---
+++ lesson/html/syain/post_data.php
@@ -0,0 +1,40 @@
+<?php
+ini_set('display_errors', "On");
+
+require_once('common.php');
+
+if(isset($_POST["status"])){
+  if (isset($_POST["id"])) {
+    $id = $_POST["id"];
+  } 
+  if (isset($_POST["name"])) {
+    $name = $_POST["name"];
+  } 
+  if (isset($_POST["age"])) {
+    $age = $_POST["age"];
+  } 
+  if (isset($_POST["work"])) {
+    $work = $_POST["work"];
+  } 
+  if (isset($_POST["old_id"])) {
+    $old_id = $_POST["old_id"];
+  }
+  if ($_POST["status"] == "create") {
+    if (check_input($id, $name, $age, $work, $error) == false) {
+      header("location: syain_create.php?error={$error}");
+      exit();
+    }
+    if ($db->idexist($id) == true) {
+      $error = "既に使用されているIDです";
+      header("location: syain_create.php?error={$error}");
+      exit();
+    }
+    if ($db->createsyain($id, $name, $age, $work) == false) {
+      $error = "DBエラー";
+      header("location: syain_create.php?error={$error}");
+      exit();
+    }
+    header("location: index.php");
+    exit();
+  }
+}
